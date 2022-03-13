7d95c574fe6daa36b623676ba0aab0b9f0ab78c5
Josedarkwood <95086650+Josedarkwood@users.noreply.github.com>
2022-03-13 13:19:47 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,84 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Taxer
+namespace Trying
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            int a = 1;
-            while( a ==1)
-            {
+            int a, b, c;
+            Console.WriteLine("Enter first Number");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Enter Second Number");
+            string input2 = Console.ReadLine();
 
-                double salary, taxrelief, taxableincome, ssnit, tax, total, net;
-                const double ssnitrate = 0.035;
-                Console.WriteLine("Enter your Salary");
-                salary = Convert.ToDouble(Console.ReadLine());
+            a = Convert.ToInt16(input1);
+            b = Convert.ToInt16(input2);
+            c = a + b;
 
-                Console.WriteLine("Enter your Tax Relief");
-                taxrelief = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("First number = " + a);
+            Console.WriteLine("Second Number = " + b);
+            Console.WriteLine(a+ " + " + b +  " = " + c);
+            Console.ReadLine();
 
-                ssnit = salary * ssnitrate;
-
-
-                taxableincome = salary - (taxrelief + ssnit);
-
-                if (taxableincome <= 365)
-                {
-                    tax = 365 * 0;
-                }
-
-                else
-                    if (taxableincome <= 475)
-                {
-                    tax = 0 + ((taxableincome - 365) * 0.05);
-                }
-                else
-                    if (taxableincome <= 605)
-                {
-                    tax = 0 + 5.5 + ((taxableincome - 475) * 0.1);
-
-                }
-
-                else
-                    if (taxableincome <= 3605)
-                {
-                    tax = 0 + 5.5 + 13.00 + ((taxableincome - 605) * 0.175);
-
-
-                }
-
-                else
-                    if (taxableincome <= 20000)
-                {
-                    tax = 0 + 5.5 + 13.00 + 525 + ((taxableincome - 3605) * 0.25);
-
-
-                }
-
-                else
-                {
-                    tax = 0 + 5.5 + 13.00 + 525 + 4098.75 + ((taxableincome - 20000) * 0.30);
-
-
-                }
-                total = ssnit + tax;
-                net = salary - total;
-
-                Console.WriteLine("--------------------------------------------------------------------------------");
-                Console.WriteLine("Basic Salary = " + (salary).ToString("c") + "\nTax Relief =  " + (taxrelief).ToString("c") + "\nSSNIT = " + (ssnit).ToString("c") +"\nTaxable Income = " +taxableincome.ToString("c")  +"\nIncome Tax = " + (tax).ToString("c") + "\nTotal Deduction = " + (total).ToString("c") + "\nNet Salary = " + (net).ToString("c"));
-
-                Console.WriteLine("--------------------------------------------------------------------------------");
-                Console.WriteLine("Enter 1 to Continue");
-                    a = Convert.ToInt16(Console.ReadLine());
-            }
-            
-            
-               
         }
-
-      
     }
 }
